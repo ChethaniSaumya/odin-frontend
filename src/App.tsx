666cecfe480ab89c0540c44cf90a9bbc92deb826
Chethani Saumya <105449384+ChethaniSaumya@users.noreply.github.com>
2025-12-21 14:13:36 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Mint from './pages/Mint'; // Import the Mint component
 import './index.css'; // CRITICAL: Import CSS here
+import HamburgerMenu from './components/HamburgerMenu';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <HamburgerMenu />
       <Mint />
     </div>
   );
