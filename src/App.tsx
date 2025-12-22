9c547927325a4c8b16fe6cee7b47ac4e66dfba30
Chethani Saumya <105449384+ChethaniSaumya@users.noreply.github.com>
2025-12-23 02:40:17 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,13 @@ import React from 'react';
 import Mint from './pages/Mint'; // Import the Mint component
 import './index.css'; // CRITICAL: Import CSS here
 import HamburgerMenu from './components/HamburgerMenu';
-import { HelmetProvider } from 'react-helmet-async';
 
 const App: React.FC = () => {
   return (
-    
-    <HelmetProvider> {/* Wrap the ENTIRE app here */}
-      <div className="App">
-        <HamburgerMenu />
-        <Mint />
-      </div>
-    </HelmetProvider> 
+    <div className="App">
+      <HamburgerMenu />
+      <Mint />
+    </div>
   );
 };
 
